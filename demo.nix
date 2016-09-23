e32ced8c5cefa3473fa64b664beae22e545b5e86
David Johnson <djohnson.m@gmail.com>
2016-09-23 13:25:59 -0500

    updated job set

--- demo.nix
+++ demo.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, stdenv, base, servant-server, wai, warp, aeson }:
 mkDerivation {
   pname = "demo";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base servant-server wai warp aeson ];
   testHaskellDepends = [ base ];
   license = stdenv.lib.licenses.bsd3;
 }
--- exe/Main.hs
+++ exe/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
+import Servant.Server
+import Servant.API
+import Data.Aeson
+import Network.Wai.Handler.Warp
+import Data.Proxy
+
 main :: IO ()
-main = pure ()
+main = do
+  putStrLn "running on 3000"
+  run 3000 $ serve (Proxy :: Proxy App) $ pure (Foo "foo")
+
+type App = Get '[JSON] Foo
+
+newtype Foo = Foo String
+  deriving (ToJSON, FromJSON)
+
+
